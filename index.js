80d0e245299a564912af060856792cd672db2971
ds-upin <divyanshu.singh.bzzf@gmail.com>
2024-11-20 20:11:54 +0530

    initial

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
+const socket = require('socket.io');
 const imageRouter = require('./router/images');
 const adminRouter = require('./router/adminRoute');
 const path = require('path');
@@ -9,7 +9,11 @@ const animaldb = require('./models/animals');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -32,8 +36,26 @@ app.get('/membership',(req,res)=>{ res.render('membership')});
 //app.get('/adoptions',(req,res)=> res.render('adoptions'));
 app.get('/About_Us',(req,res)=> res.render('About_Us'));
 app.get('/video',(req,res)=>{res.render('')});
+app.get('/animal_info/:id', async (req, res) => {
+  const info = req.params.id;
+  
+  try {
+    const dd = await animaldb.find({img: info});
+    
+    if (dd.length === 0) {
+      return res.json({'msg':"wrong"});
+    }
+    console.log(dd);
+    res.render('animal_info', {dd});
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error retrieving data from the database.");
+  }
+});
 
-
+app.get('/connect',(req,res)=>{
+  res.render('video');
+});
 
 app.get('/adopt/:id',async (req,res)=>{
   const id = req.params.id;
@@ -77,35 +99,43 @@ app.get('/animals', async (req, res) => {
 });
 
 
+let io = socket(server);
+io.on("connection", function (socket) {
+  console.log("User Connected :" + socket.id);
+  socket.on("join", function (roomName) {
+    let rooms = io.sockets.adapter.rooms;
+    let room = rooms.get(roomName);
+    if (room == undefined) {
+      socket.join(roomName);
+      socket.emit("created");
+    } else if (room.size == 1) {
+      socket.join(roomName);
+      socket.emit("joined");
+    } else {
+      socket.emit("full");
+    }
+    console.log(rooms);
+  });
 
-let users = [];
+  socket.on("ready", function (roomName) {
+    socket.broadcast.to(roomName).emit("ready"); 
+  });
 
-io.on('connection', (socket) => {
-  console.log('A new user connected:', socket.id);
-  users.push(socket.id);
-  socket.broadcast.emit('new-user', socket.id);
-  socket.emit('existing-users', users.filter(id => id !== socket.id));
-  socket.on('start-broadcast', () => {
-    console.log(`${socket.id} started broadcasting.`);
-    socket.broadcast.emit('new-user', socket.id); 
+  socket.on("candidate", function (candidate, roomName) {
+    console.log(candidate);
+    socket.broadcast.to(roomName).emit("candidate", candidate); 
   });
-  socket.on('send-offer', (remoteSocketId, offer) => {
-    io.to(remoteSocketId).emit('receive-offer', socket.id, offer);
+
+  socket.on("offer", function (offer, roomName) {
+    socket.broadcast.to(roomName).emit("offer", offer); 
   });
-  socket.on('send-answer', (remoteSocketId, answer) => {
-    io.to(remoteSocketId).emit('receive-answer', socket.id, answer);
-  });
-  socket.on('send-ice-candidate', (remoteSocketId, candidate) => {
-    io.to(remoteSocketId).emit('receive-ice-candidate', socket.id, candidate);
-  });
-  socket.on('disconnect', () => {
-    console.log('User disconnected:', socket.id);
-    users = users.filter(id => id !== socket.id);
-    socket.broadcast.emit('user-disconnected', socket.id); 
+
+  socket.on("answer", function (answer, roomName) {
+    socket.broadcast.to(roomName).emit("answer", answer);
   });
 });
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// server.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
---
+++ public/animal_info.js
@@ -0,0 +1,10 @@
+ contact_btn = document.querySelector(".contact_button");
+ video_contact_btn = document.querySelector(".video_contact_button");
+
+ contact_btn.addEventListener("click",()=>{
+        window.location.href="https://whatsapp.com/channel/0029VaryIauB4hdUFeadKD2s";
+ });         
+ 
+ video_contact_btn.addEventListener("click" , ()=>{
+    window.location.href="./video.html";
+ });
